dfbb7576a423b6c8320259da95217318339a31fa
parksb <parkgds@gmail.com>
2023-04-27 12:41:35 +0900

    메인에서 Map으로 이동하는 임시 버튼 추가

--- mobile/app/src/main/java/com/safe_bicycle_assistant/s_ba/MainActivity.java
+++ mobile/app/src/main/java/com/safe_bicycle_assistant/s_ba/MainActivity.java
@@ -2,7 +2,9 @@ package com.safe_bicycle_assistant.s_ba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openMap(View view) {
+        Intent mapIntent = new Intent(MainActivity.this, MapActivity.class);
+        MainActivity.this.startActivity(mapIntent);
     }
 }
